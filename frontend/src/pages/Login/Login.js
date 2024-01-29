0f97f943eb2c52f8fc50985087ce1e79fd5f1f10
juliamellolopes <juliamellolopesgoncalves@gmail.com>
2024-01-29 11:25:24 -0300

    css login

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,45 +1,57 @@
 import React, { useState } from "react";
 import "../../styles/Login.css"; // Certifique-se de que este caminho está correto
 import userService from "../../services/userService"; // Seu caminho real para userService
-import logoImage from "../../assets/logo.png";
+import showPasswordIcon from "../../assets/Icons/showPassword.png";
+import hidePasswordIcon from "../../assets/Icons/hidePassword.png";
 
 function Login({ onLogin }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [remember, setRemember] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [hidePassword, setHidePassword] = useState(hidePasswordIcon);
 
   const handleLogin = async () => {
     try {
-      // Faz a chamada para o serviço de login
       const data = await userService.login(email, password);
-
-      // Se a autenticação for bem-sucedida, chama a função onLogin
-      // Pode-se passar informações do usuário, se necessário
-      onLogin(data); // Aqui você pode passar informações do usuário se necessário
     } catch (error) {
-      console.error(error.message); // Lida com erros de autenticação
-      // Aqui você pode exibir uma mensagem para o usuário, por exemplo:
-      // setError("Falha ao fazer login. Verifique suas credenciais.");
+      console.error(error.message);
     }
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+    setHidePassword(showPassword ? hidePasswordIcon : showPasswordIcon);
   };
 
   return (
     <div className="login-container">
       <div className="login-form">
-        <h1>Bem Vindo</h1>
         <div className="login-acess">
+          <h1>Bem Vindo</h1>
           <input
             type="email"
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <div className="password-input">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <span
+              className="toggle-password-button"
+              onClick={togglePasswordVisibility}
+            >
+              <img
+                src={hidePassword}
+                alt={showPassword ? "Ocultar senha" : "Mostrar senha"}
+              />
+            </span>
+          </div>
           <div className="login-remember">
             <input
               type="checkbox"
@@ -48,19 +60,15 @@ function Login({ onLogin }) {
               onChange={(e) => setRemember(e.target.checked)}
             />
             <label htmlFor="remember">Remember</label>
+            <a href="#" className="forgot-password">
+              forgot password?
+            </a>
           </div>
-          <a href="#" className="forgot-password">
-            forgot password?
-          </a>
           <button onClick={handleLogin}>Log in</button>
         </div>
         <div className="login-branding">
           <div className="brand-logo"> </div>
-          <p className="brand-slogan">
-            O VENDEMOS PAPEL,
-            <br />
-            VENDEMOS SOLUÇÃO.
-          </p>
+          <div className="brand-slogan"> </div>
         </div>
       </div>
     </div>
